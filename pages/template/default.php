4ae909721efef7461ebd437b3ca7e098d4afd1c2
oggixtag <andreaoggix@gmail.com>
2025-10-28 08:12:34 +0100

    Add files via upload
    Using static class.
    Article and Category are inherited from Table.
    Add class Utility.
    Structured for articles and categories page.
    Add join on db between tables articles and categories.

--- pages/template/default.php
+++ pages/template/default.php
@@ -1,10 +1,14 @@
+<?
+
+//use NsAppBlog\App;
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?= \NsAppBlog\App::getTitleSite() ?></title>
 </head>
 
 <body>
